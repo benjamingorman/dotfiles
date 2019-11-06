5404a8a38dbdc75be920f800b43370bd06ed2483
Ben Gorman <beng@speechmatics.com>
2019-11-06 14:39:23 +0000

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 apt update
 apt install -y vim curl git tmux
 curl https://raw.githubusercontent.com/benjamingorman/dotfiles/master/.vimrc \
-    --output $HOME/.tmux.conf
-curl https://raw.githubusercontent.com/benjamingorman/dotfiles/master/.tmux.conf \
     --output $HOME/.vimrc
+curl https://raw.githubusercontent.com/benjamingorman/dotfiles/master/.tmux.conf \
+    --output $HOME/.tmux.conf
